05e23f4e528f55c1a27c03db6a8c0ea376672d6f
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-05-18 09:14:00 +0700

    test sysinfo on main project

--- main.go
+++ main.go
@@ -6,22 +6,37 @@ import (
 
 	pci_dss_commons "github.com/purpeltim/scm/standards/readers/pcissc/pcidss/standards/commons"
 	"github.com/purpeltim/scm/standards/util"
+	"github.com/purpeltim/scm/tools/sysinfo"
 )
 
 func main() {
 	// fmt.Println("test")
 
+	type TestProject struct {
+		HostInfo  *sysinfo.HostInfo
+		PciDssStd *pci_dss_commons.Standard
+	}
+
+	T := TestProject{}
+
 	std := pci_dss_commons.Standard{}
 	pci_dss_commons.CfgDir = "standards/definitions/pcissc/pcidss/standards/"
+
 	result, err := std.ReadStandard("3.2.1")
 	if err != nil {
 		util.ExitWithError(err)
 	}
 
-	pciStd, err := json.Marshal(result)
+	T.PciDssStd = result
+	T.HostInfo, err = sysinfo.GetHostInfo()
 	if err != nil {
 		util.ExitWithError(err)
 	}
-	fmt.Printf("%s", pciStd)
+
+	TestPciStd, err := json.Marshal(T)
+	if err != nil {
+		util.ExitWithError(err)
+	}
+	fmt.Printf("%s", TestPciStd)
 
 }
