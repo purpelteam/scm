afece4b0723d4de825ce0d96d5de6be33d22315c
Muhammad Hari Suharto <muharihar@infoterkini.com>
2020-06-01 06:52:10 +0700

    set default command to `help`

--- cmd/root.go
+++ cmd/root.go
@@ -5,21 +5,26 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spf13/cobra"
 	"github.com/golang/glog"
+	"github.com/spf13/cobra"
 )
 
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "scm",
 	Short: "SCM is a simple Security Control Management tools",
-	Long: `A Simple and Lite Security Control Management built with
-				  love by purpeltim and friends in Go.
-				  Complete documentation is available at http://github.com/purpeltim/scm`,
+	Long: `
+A Simple Security Control Management Tools
+built with love by purpeltim and friends in Go.
+Complete documentation is available at https://github.com/purpeltim/scm`,
 	Run: func(cmd *cobra.Command, args []string) {
-	  // Do Stuff Here
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		// Do Stuff Here
 	},
-  }
+}
 
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
